f2ad3f16d78b4e95b4a8333659bdb1203888a0db
Hector Rios <oj.rios@gmail.com>
2025-03-19 22:30:26 +0100

    start initial implementation of the scan function

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,40 @@ use tokio::net::TcpStream;
 use tokio::task;
 
 const MAX: u16 = 65535;
-const IPFALLBACK: IpAddr = IpAddr::V4(Ipv4Addr::new(127, 0,0,1));
+const IPFALLBACK: IpAddr = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
 
 #[derive(Debug, Clone, Bpaf)]
 pub struct Arguments {
     #[bpaf(long, short, argument("Address"), fallback(IPFALLBACK))]
     pub address: IpAddr,
-    #[bpaf(long("start"), short('s'), guard(start_port_guard, "Must be greater than 0"), fallback(1u16))]
+    #[bpaf(
+        long("start"),
+        short('s'),
+        guard(start_port_guard, "Must be greater than 0"),
+        fallback(1u16)
+    )]
     pub start_port: u16,
-    #[bpaf(long("end"), short('e'), guard(end_port_guard, "Must be less than or equal to 65535"), fallback(MAX))]
+    #[bpaf(
+        long("end"),
+        short('e'),
+        guard(end_port_guard, "Must be less than or equal to 65535"),
+        fallback(MAX)
+    )]
     pub end_port: u16,
 }
 
-
+async fn scan(tx: Sender<u16>, start_port: u16, addr: IpAddr) {
+    //attempt to establish a TCP connection to the specified
+    //`addr` and `start_port`
+    match TcpStream::connect(format!("{}:{}", addr, start_port)).await {
+        Ok(_) => {
+            print!(".");
+            io::stdout().flush().unwrap();
+            tx.send(start_port).unwrap();
+        }
+        Err(_) => {}
+    }
+}
 
 fn main() {
     println!("Hello, world!");
